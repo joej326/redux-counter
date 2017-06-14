2196b70b8d92fc51792db1860c36578811440df6
Joe <joe@Joes-MacBook-Pro.local>
2017-06-14 15:59:47 -0600

    added comments

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,6 +1,9 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
 
+//------------------
+// actions
+//------------------
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 
@@ -20,30 +23,30 @@ export function decrement(amt) {
   }
 }
 
+//-----------------//
 var initialState = {
   counter: 0 //pro tip -- always add a value here when you make an initialState
 }
 
-export default  function reducer (state = initialState, action){  //in es6 use equals in a paramater to set a backup value if value passed in is falsey
+export default function reducer(state = initialState, action) { //in es6 use equals in a paramater to set a backup value if value passed in is falsey
 
-  state = Object.assign(state, {
-    lastAction: action
-  })
-
-  switch (action.type) {
-    case INCREMENT:
-    return Object.assign({}, state, {
-      counter: state.counter + action.payload
+    state = Object.assign(state, {
+        lastAction: action
     })
-    break;
-    case DECREMENT:
-    return Object.assign({}, state, {
-      counter: state.counter - action.payload
-  })
-  return state;
-}
-}
 
+    switch (action.type) {
+        case INCREMENT:
+            return Object.assign({}, state, {
+                counter: state.counter + action.payload
+            })
+            break;
+        case DECREMENT:
+            return Object.assign({}, state, {
+                counter: state.counter - action.payload
+            })
+            return state;
+    }
+}
 
 
 
